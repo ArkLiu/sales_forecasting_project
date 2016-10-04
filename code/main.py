7f31ebdfe6b42c755fb0827a217e72d0dc9a7f1e
ArkLiu <ark@Arks-MacBook-Pro.local>
2016-10-04 12:12:12 -0700

    change the input / output file name

--- code/main.py
+++ code/main.py
@@ -4,10 +4,10 @@ import pandas as pd
 from alsofit import AlsoFit
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/also_fit_record.csv')
+    df = pd.read_csv('../data/also_fit_raw.csv')
 
     also_fit = AlsoFit()
     # also_fit.load_data(df.ix[:(df.shape[0]/2.0), :])
     also_fit.load_data(df)
     also_fit.transform()
-    also_fit.df.to_csv('../data/also_fit_record_new.csv')
+    also_fit.df.to_csv('../data/also_fit_clean.csv')
