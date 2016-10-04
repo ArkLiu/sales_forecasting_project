2130512a2677fb3e1eed5cd63b148b4d47e2da0a
ArkLiu <ark@Arks-MacBook-Pro.local>
2016-10-04 11:51:50 -0700

    working on also fit feature

---
+++ code/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from alsofit import AlsoFit
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/also_fit_record.csv')
+
+    also_fit = AlsoFit()
+    # also_fit.load_data(df.ix[:(df.shape[0]/2.0), :])
+    also_fit.load_data(df)
+    also_fit.transform()
+    also_fit.df.to_csv('../data/also_fit_record_new.csv')
