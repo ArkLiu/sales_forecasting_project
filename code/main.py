21341fdfb6d85fa119105ba4f977f52b8a952eee
ArkLiu <ark@Arks-MacBook-Pro.local>
2016-10-05 13:26:01 -0700

    add also fit csv. feature engineering on training period

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import KFold, cross_val_score
 
 from alsofit import AlsoFit
 
@@ -13,19 +14,49 @@ if __name__ == '__main__':
     # also_fit.transform()
     # also_fit.df.to_csv('../data/also_fit_clean.csv')
 
-    df_a = pd.read_csv('../data/also_fit_clean.csv')
-    df_s = pd.read_csv('../data/sales_data_clean_9_train_period.csv')
+    df_a_1 = pd.read_csv('../data/also_fit_clean_1yr_bin.csv')
+    df_a_2 = pd.read_csv('../data/also_fit_clean_5yr_bin.csv')
+    df_a_3 = pd.read_csv('../data/also_fit_clean_10yr_bin.csv')
+    df_s = pd.read_csv('../data/6m.csv')
 
-    df_a.set_index(keys=df_a.ITEM_NUMBER, drop=False, inplace=True)
+    df_a_1.set_index(keys=df_a_1.ITEM_NUMBER, drop=False, inplace=True)
+    df_a_2.set_index(keys=df_a_2.ITEM_NUMBER, drop=False, inplace=True)
+    df_a_3.set_index(keys=df_a_3.ITEM_NUMBER, drop=False, inplace=True)
     df_s.set_index(keys=df_s.ITEM_NUMBER, drop=True, inplace=True)
-    X = df_a.join(df_s, how='inner', rsuffix='_x')
-    X.reset_index(drop=True, inplace=True)
-    X.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x'], axis=1, inplace=True)
-    y = X.pop('CM')
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.1)
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     X, y, test_size=0.1, random_state=42)
+    X_1 = df_a_1.join(df_s, how='inner', rsuffix='_x')
+    X_2 = df_a_2.join(df_s, how='inner', rsuffix='_x')
+    X_3 = df_a_3.join(df_s, how='inner', rsuffix='_x')
+    X_1.reset_index(drop=True, inplace=True)
+    X_2.reset_index(drop=True, inplace=True)
+    X_3.reset_index(drop=True, inplace=True)
+    X_1.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x'], axis=1, inplace=True)
+    X_2.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x'], axis=1, inplace=True)
+    X_3.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x'], axis=1, inplace=True)
+    y_1 = X_1.pop('CM')
+    y_2 = X_2.pop('CM')
+    y_3 = X_3.pop('CM')
+
     rf = RandomForestRegressor(n_estimators=10)
-    rf.fit(X_train, y_train)
-    print 'RandomForestRegressor R2 Score: ', rf.score(X_test, y_test)
+
+    k_fold = KFold(n_splits=10, shuffle=True)
+    cvs = cross_val_score(rf, X_1, y_1, cv=k_fold, n_jobs=-1)
+    # cvs = cross_val_score(rf, X.ix[:,-12:], y, cv=k_fold, n_jobs=-1)
+    print cvs
+    print cvs.mean()
+    print cvs.std()
+
+    cvs = cross_val_score(rf, X_2, y_2, cv=k_fold, n_jobs=-1)
+    # cvs = cross_val_score(rf, X.ix[:,-12:], y, cv=k_fold, n_jobs=-1)
+    print cvs
+    print cvs.mean()
+    print cvs.std()
+
+    cvs = cross_val_score(rf, X_3, y_3, cv=k_fold, n_jobs=-1)
+    # cvs = cross_val_score(rf, X.ix[:,-12:], y, cv=k_fold, n_jobs=-1)
+    print cvs
+    print cvs.mean()
+    print cvs.std()
+
+    # rfr = RandomForestRegressor()
+    # rfr.fit(X, y)
+    # print rfr.feature_importances_
