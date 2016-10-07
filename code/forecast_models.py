73646e08787af0dc5d55736b4aedb2f540ea4310
ArkLiu <ark@Arks-MacBook-Pro.local>
2016-10-07 09:43:05 -0700

    TGIF - still working on score system..

--- code/forecast_models.py
+++ code/forecast_models.py
@@ -25,20 +25,35 @@ class MSF(object):
         pass
 
     def predict(self, df):
-        return df.mean(axis=1).values
+        return df.mean(axis=1)
 
 
 class Score(object):
 
     def __init__(self):
-        pass
+        self.X_true = pd.DataFrame()
+        self.y_true = pd.Series()
+        self.item_cols = ['RANK_A', 'RANK_B', 'RANK_C', 'RANK_N']
 
-    def rmse_score(self, y_true, y_predict):
-        return np.sqrt(((y_true - y_predict) ** 2).mean())
+    def load_data(self, df, series):
+        self.X_true = df
+        self.y_true = series
 
-    def r2_score(self, y_true, y_predict):
-        return 1 - (((y_true - y_predict)**2).sum() /
-                    ((y_true - y_true.mean())**2).sum())
+    def rmse_score(self, y_true, y_pred):
+        return round(np.sqrt(((y_true - y_pred) ** 2).mean()), 3)
 
+    def r2_score(self, y_true, y_pred):
+        return round(1 - (((y_true - y_pred)**2).sum() /
+                          ((y_true - y_true.mean())**2).sum()), 3)
+
+    def item_rank_score(self, y_pred_1, y_pred_2):
+        for col in self.item_cols:
+            mask = self.X_true[col] == 1
+            # mask 2 = model B is doing better than model A
+            mask2 = abs(
+                y_pred_1 - self.y_true) > abs(y_pred_2 - self.y_true)
+            print col, ' Model A | Model B | TOTAL:{}'.format(mask.sum())
+            print 'More Close:', mask.sum() - (mask & mask2).sum(), (mask & mask2).sum()
+            print 'RMSE:',  self.rmse_score(self.y_true[mask], y_pred_1[mask]), self.rmse_score(self.y_true[mask], y_pred_2[mask])
 if __name__ == '__main__':
     pass
--- code/main.py
+++ code/main.py
@@ -5,46 +5,52 @@ from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from also_fit import AlsoFit
 from forecast_models import MSF, Score
 
+
+def transform(a, b):
+    a.set_index(keys=a.ITEM_NUMBER, drop=False, inplace=True)
+    b.set_index(keys=b.ITEM_NUMBER, drop=True, inplace=True)
+    X = a.join(b, how='inner', rsuffix='_x')
+    X.reset_index(drop=True, inplace=True)
+    X.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x', 'CM'], axis=1, inplace=True)
+    y = X.pop('NM')
+    return X, y
+
 if __name__ == '__main__':
+    also_fit_path = '../data/also_fit_clean.csv'
+    # train_file_path = '../data/train_data_predict_2016_06_3m.csv'
+    # test_file_path = '../data/test_data_predict_2016_07_1m.csv'
+    train_file_path = '../data/predict_2016_03_1m.csv'
+    test_file_path = '../data/predict_2016_04_1m.csv'
 
     # print "fitting train data"
-    df_a = pd.read_csv('../data/also_fit_clean.csv', low_memory=False)
-    df_s = pd.read_csv(
-        '../data/train_data_predict_5br_2016_06_3m.csv', low_memory=False)
-    df_a.set_index(keys=df_a.ITEM_NUMBER, drop=False, inplace=True)
-    df_s.set_index(keys=df_s.ITEM_NUMBER, drop=True, inplace=True)
-    X_train = df_a.join(df_s, how='inner', rsuffix='_x')
-    X_train.reset_index(drop=True, inplace=True)
-    X_train.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x', 'CM'], axis=1, inplace=True)
-    y_train = X_train.pop('NM')
+    df_a = pd.read_csv(also_fit_path, low_memory=False)
+    df_s = pd.read_csv(train_file_path, low_memory=False)
+    X_train, y_train = transform(df_a, df_s)
 
     # print "fitting test data"
-    df_a = pd.read_csv('../data/also_fit_clean.csv', low_memory=False)
-    df_s = pd.read_csv(
-        '../data/test_data_predict_5br_2016_07_1m.csv', low_memory=False)
-    df_a.set_index(keys=df_a.ITEM_NUMBER, drop=False, inplace=True)
-    df_s.set_index(keys=df_s.ITEM_NUMBER, drop=True, inplace=True)
-    X_test = df_a.join(df_s, how='inner', rsuffix='_x')
-    X_test.reset_index(drop=True, inplace=True)
-    X_test.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x', 'CM'], axis=1, inplace=True)
-    y_test = X_test.pop('NM')
+    df_a = pd.read_csv(also_fit_path, low_memory=False)
+    df_s = pd.read_csv(test_file_path, low_memory=False)
+    X_test, y_test = transform(df_a, df_s)
 
     model = RandomForestRegressor(n_estimators=50, n_jobs=-1)
     # model = GradientBoostingRegressor(n_estimators=100)
 
     model.fit(X_train, y_train)
     model_pred = model.predict(X_test)
+    model_pred = pd.Series(model_pred)
 
     msf_X = X_test.ix[:, -12:]
     msf = MSF()
     msf_pred = msf.predict(msf_X)
 
     s = Score()
+    s.load_data(X_test, y_test)
     print "MSF rmse_score: {}".format(s.rmse_score(y_test, msf_pred))
     print "model rmse_score: {}".format(s.rmse_score(y_test, model_pred))
-    print
     print "MSF r2_score: {}".format(s.r2_score(y_test, msf_pred))
     print "model r2_score: {}".format(s.r2_score(y_test, model_pred))
     print
-    for x in sorted(zip(X_train.columns, model.feature_importances_), reversed=True)[:10]:
-        print x[0], x[1]
+
+    s.item_rank_score(msf_pred, model_pred)
+    # for x in sorted(zip(model.feature_importances_, X_train.columns), reverse=True)[:20]:
+    #     print x[1], x[0]
