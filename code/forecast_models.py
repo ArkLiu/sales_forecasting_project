c825c414d0ddb5c43c44fab80c0a47f70f4774b2
ArkLiu <ark@Arks-MacBook-Pro.local>
2016-10-10 16:27:09 -0700

    working on the MAD forecast measurement

--- code/forecast_models.py
+++ code/forecast_models.py
@@ -15,8 +15,10 @@ FUNCTIONS:
     strptime -- Calculates the time struct represented by the passed-in string
 
 """
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sb
 
 
 class MSF(object):
@@ -31,29 +33,69 @@ class MSF(object):
 class Score(object):
 
     def __init__(self):
-        self.X_true = pd.DataFrame()
-        self.y_true = pd.Series()
-        self.item_cols = ['RANK_A', 'RANK_B', 'RANK_C','RANK_D', 'RANK_N']
+        self.X_test = pd.DataFrame()
+        self.y_test = pd.Series()
+        self.item_cols = ['RANK_A', 'RANK_B', 'RANK_C', 'RANK_D', 'RANK_N']
 
     def load_data(self, df, series):
-        self.X_true = df
-        self.y_true = series
+        self.X_test = df
+        self.y_test = series
 
-    def rmse_score(self, y_true, y_pred):
+    def rmse_score(self, y_pred):
+        y_pred = y_pred.apply(lambda x: round(x))
+        return round(np.sqrt(((self.y_test - y_pred) ** 2).mean()), 3)
+
+    def _rmse_score(self, y_true, y_pred):
+        y_pred = y_pred.apply(lambda x: round(x))
         return round(np.sqrt(((y_true - y_pred) ** 2).mean()), 3)
 
-    def r2_score(self, y_true, y_pred):
-        return round(1 - (((y_true - y_pred)**2).sum() /
-                          ((y_true - y_true.mean())**2).sum()), 3)
+    def r2_score(self,  y_pred):
+        y_pred = y_pred.apply(lambda x: round(x))
+        r2 = round(1 - (((self.y_test - y_pred)**2).sum() /
+                        ((self.y_test - self.y_test.mean())**2).sum()), 3)
+        return r2
+
+    def _r2_score(self, y_true, y_pred):
+        y_pred = y_pred.apply(lambda x: round(x))
+        r2 = round(1 - (((y_true - y_pred)**2).sum() /
+                        ((y_true - y_true.mean())**2).sum()), 3)
+        return r2
+
+    def adj_r2_score(self, y_pred):
+        r2 = self.r2_score(y_pred)
+        p, N = self.X_test.shape[1], self.X_test.shape[0]
+        adj_r2 = round(1 - ((1 - r2) * (N - 1) / (N - p - 1)), 3)
+        return adj_r2
 
     def item_rank_score(self, y_pred_1, y_pred_2):
+        y_pred_1 = y_pred_1.apply(lambda x: round(x))
+        y_pred_2 = y_pred_2.apply(lambda x: round(x))
         for col in self.item_cols:
-            mask = self.X_true[col] == 1
+            mask = self.X_test[col] == 1
             # mask 2 = model B is doing better than model A
             mask2 = abs(
-                y_pred_1 - self.y_true) > abs(y_pred_2 - self.y_true)
+                y_pred_1 - self.y_test) > abs(y_pred_2 - self.y_test)
             print col, ' Model A | Model B | TOTAL:{}'.format(mask.sum())
             print 'More Close:', mask.sum() - (mask & mask2).sum(), (mask & mask2).sum()
-            print 'RMSE:',  self.rmse_score(self.y_true[mask], y_pred_1[mask]), self.rmse_score(self.y_true[mask], y_pred_2[mask])
+            print 'RMSE:',  self._rmse_score(self.y_test[mask], y_pred_1[mask]), self._rmse_score(self.y_test[mask], y_pred_2[mask])
+
+    def hist(self, y_pred, label):
+        y_pred = y_pred.apply(lambda x: round(x))
+        diff = y_pred - self.y_test
+        mask = (diff >= 50) | (diff <= -50)
+        print "outlier: {}".format(mask.sum())
+        # plt.hist(diff, bins=100, range=(-50, 50),
+        #          histtype='step', normed=True, label=label)
+        plt.hist(diff, bins=100, range=(-50, 50),
+                 histtype='bar', stacked=True, label=label)
+
+    def plot_2(self, y_pred_1, y_pred_2):
+        y_pred_1 = y_pred_1.apply(lambda x: round(x))
+        y_pred_2 = y_pred_2.apply(lambda x: round(x))
+        x = range(len(self.y_test))
+        plt.plot(x,y_pred_1 -self.y_test,c='r',label='model_1')
+        plt.plot(x,y_pred_2 -self.y_test,c='g',label='model_2')
+        plt.ylim(-50,50)
+
 if __name__ == '__main__':
     pass
--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sb
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 
 from also_fit import AlsoFit
@@ -11,58 +13,101 @@ def transform(a, b):
     b.set_index(keys=b.ITEM_NUMBER, drop=True, inplace=True)
     X = a.join(b, how='inner', rsuffix='_x')
     X.reset_index(drop=True, inplace=True)
+    output_df = X.ix[:, ['ITEM_NUMBER', 'BR_ID',
+                         'RANK_A', 'RANK_B', 'RANK_C', 'RANK_D', 'RANK_N', 'NM']]
     X.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x', 'CM'], axis=1, inplace=True)
-    X['L12-10M'] = X.ix[:, 'L12M':'L11M'].mean(axis=1)
-    X['L10-9M'] = X.ix[:, 'L10M':'L9M'].mean(axis=1)
-    X['L8-7M'] = X.ix[:, 'L8M':'L7M'].mean(axis=1)
-    X['L6-5M'] = X.ix[:, 'L6M':'L5M'].mean(axis=1)
-    X['L14-3M'] = X.ix[:, 'L4M':'L3M'].mean(axis=1)
-    X['L2-1M'] = X.ix[:, 'L2M':'L1M'].mean(axis=1)
-    X['L12-10M'] = X.ix[:, 'L12M':'L10M'].mean(axis=1)
-    X['L9-7M'] = X.ix[:, 'L9M':'L7M'].mean(axis=1)
-    X['L7-4M'] = X.ix[:, 'L7M':'L4M'].mean(axis=1)
-    X['L3-1M'] = X.ix[:, 'L3M':'L1M'].mean(axis=1)
-    X['L12M_MEAN'] = X.ix[:, 'L12M':'L1M'].mean(axis=1)
+
+    # X['L12-11_S'] = X.ix[:, 'L12M':'L11M'].sum(axis=1)
+    # X['L10-9_S'] = X.ix[:, 'L10M':'L9M'].sum(axis=1)
+    # X['L8-7M_S'] = X.ix[:, 'L9M':'L7M'].sum(axis=1)
+    # X['L6-5M_S'] = X.ix[:, 'L6M':'L5M'].sum(axis=1)
+    # X['L4-3_S'] = X.ix[:, 'L4M':'L3M'].sum(axis=1)
+    # X['L2-1_S'] = X.ix[:, 'L2M':'L1M'].sum(axis=1)
+    #
+    # X['Q4_S'] = X.ix[:, 'L12M':'L10M'].sum(axis=1)
+    # X['Q3_S'] = X.ix[:, 'L9M':'L7M'].sum(axis=1)
+    # X['Q2_S'] = X.ix[:, 'L7M':'L4M'].sum(axis=1)
+    # X['Q1_S'] = X.ix[:, 'L3M':'L1M'].sum(axis=1)
+    #
+    # X['L12-10_M'] = X.ix[:, 'L12M':'L11M'].mean(axis=1)
+    # X['L10-9_M'] = X.ix[:, 'L10M':'L9M'].mean(axis=1)
+    # X['L8-7_M'] = X.ix[:, 'L8M':'L7M'].mean(axis=1)
+    # X['L6-5_M'] = X.ix[:, 'L6M':'L5M'].mean(axis=1)
+    # X['L4-3_M'] = X.ix[:, 'L4M':'L3M'].mean(axis=1)
+    # X['L2-1_M'] = X.ix[:, 'L2M':'L1M'].mean(axis=1)
+    #
+    # X['Q4_M'] = X.ix[:, 'L12M':'L10M'].mean(axis=1)
+    # X['Q3_M'] = X.ix[:, 'L9M':'L7M'].mean(axis=1)
+    # X['Q2_M'] = X.ix[:, 'L7M':'L4M'].mean(axis=1)
+    # X['Q1_M'] = X.ix[:, 'L3M':'L1M'].mean(axis=1)
+    # X['12M_M'] = X.ix[:, 'L12M':'L1M'].mean(axis=1)
+
     y = X.pop('NM')
-    return X, y
+    return X, y, output_df
+
+
+def round_number(series):
+    return series.apply(lambda x: round(x))
 
 if __name__ == '__main__':
+    train_list = ['/Users/ark/Dropbox/sales_data/201605.csv',
+                  '/Users/ark/Dropbox/sales_data/201606.csv', '/Users/ark/Dropbox/sales_data/201607.csv']
+    test_list = ['/Users/ark/Dropbox/sales_data/201607.csv',
+                 '/Users/ark/Dropbox/sales_data/201608.csv', '/Users/ark/Dropbox/sales_data/201609.csv']
     also_fit_path = '../data/also_fit_clean.csv'
-    train_file_path = '~/Dropbox/sales_data/201605.txt'
-    test_file_path  = '~/Dropbox/sales_data/201607.txt'
 
-    # print "fitting train data"
-    df_a = pd.read_csv(also_fit_path, low_memory=False)
-    df_s = pd.read_csv(train_file_path, low_memory=False)
-    # df_a = df_a[df_a.PRODUCT_LINE >= 2000]
-    # df_s.drop(df_s.ix[:, 'L24M':'L13M'], axis=1, inplace=1)
-    X_train, y_train = transform(df_a, df_s)
+    for files in zip(train_list, test_list):
+        predict_month = files[1][-10:-4]
 
-    # print "fitting test data"
-    df_a = pd.read_csv(also_fit_path, low_memory=False)
-    df_s = pd.read_csv(test_file_path, low_memory=False)
-    # df_s.drop(df_s.ix[:, 'L24M':'L13M'], axis=1, inplace=1)
-    X_test, y_test = transform(df_a, df_s)
-    # df_a = df_a[df_a.PRODUCT_LINE >= 2000]
-    model = RandomForestRegressor(n_estimators=10, n_jobs=-1)
-    # model = GradientBoostingRegressor(n_estimators=1000)
+        df_a = pd.read_csv(also_fit_path, low_memory=False)
+        df_s = pd.read_csv(files[0], low_memory=False)
+        X_train, y_train, output_df = transform(df_a, df_s)
 
-    model.fit(X_train, y_train)
-    model_pred = model.predict(X_test)
-    model_pred = pd.Series(model_pred)
+        df_a = pd.read_csv(also_fit_path, low_memory=False)
+        df_s = pd.read_csv(files[1], low_memory=False)
+        X_test, y_test, output_df = transform(df_a, df_s)
+        model = RandomForestRegressor(n_estimators=10, n_jobs=-1)
+        # model = GradientBoostingRegressor(n_estimators=1000)
 
-    msf_X = X_test.ix[:, 'L12M':'L1M']
-    msf = MSF()
-    msf_pred = msf.predict(msf_X)
+        model.fit(X_train, y_train)
+        model_pred = model.predict(X_test)
+        model_pred = pd.Series(model_pred)
 
-    s = Score()
-    s.load_data(X_test, y_test)
-    print "MSF rmse_score: {}".format(s.rmse_score(y_test, msf_pred))
-    print "model rmse_score: {}".format(s.rmse_score(y_test, model_pred))
-    print "MSF r2_score: {}".format(s.r2_score(y_test, msf_pred))
-    print "model r2_score: {}".format(s.r2_score(y_test, model_pred))
-    print
-    # Display top 30 important features
-    s.item_rank_score(msf_pred, model_pred)
-    for x in sorted(zip(model.feature_importances_, X_train.columns), reverse=True)[:30]:
-        print round(x[0], 3), x[1]
+        msf_X = X_test.ix[:, 'L12M':'L1M']
+        msf = MSF()
+        msf_pred = msf.predict(msf_X)
+
+        s = Score()
+        s.load_data(X_test, y_test)
+        print "MSF rmse_score: {}".format(s.rmse_score(msf_pred))
+        print "model rmse_score: {}".format(s.rmse_score(model_pred))
+        print
+        print "MSF r2_score: {}".format(s.r2_score(msf_pred))
+        print "model r2_score: {}".format(s.r2_score(model_pred))
+        print "model adj_r2_score: {}".format(s.adj_r2_score(model_pred))
+        print
+        # s.hist(msf_pred,'MSF')
+        # # s.hist(model_pred,'RF')
+        # s.plot_3(msf_pred, model_pred)
+        # plt.legend()
+        # plt.show()
+
+        output_df['MSF_FORECAST'] = round_number(msf_pred)
+        output_df['RF_FORECAST'] = round_number(model_pred)
+        # print output_df.head()
+        output_df.to_csv(
+            '/Users/ark/Dropbox/sales_data/{}_forecast.csv'.format(predict_month), mode='w', index=False)
+        # s.item_rank_score(msf_pred, model_pred)
+
+        # Display top 30 important features
+        # for x in sorted(zip(model.feature_importances_, X_train.columns), reverse=True)[:30]:
+        #     print round(x[0], 3), x[1]
+        # print
+
+        # Plot feature important
+        # x = xrange(len(X_test.columns))
+        # y = model.feature_importances_
+        # x_ticks = X_test.columns
+        # plt.xticks(x, x_ticks, rotation='vertical')
+        # plt.plot(x, y)
+        # plt.show()
