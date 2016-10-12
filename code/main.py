0b53b3b6e1dd19d0ad868c075f06ae73a43604f8
ArkLiu <ark@Arks-MacBook-Pro.local>
2016-10-11 23:34:25 -0700

    measeurement function is finally work - output as pv_table format

--- code/main.py
+++ code/main.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -9,39 +11,37 @@ from forecast_models import MSF, Score
 
 
 def transform(a, b):
-    a.set_index(keys=a.ITEM_NUMBER, drop=False, inplace=True)
-    b.set_index(keys=b.ITEM_NUMBER, drop=True, inplace=True)
-    X = a.join(b, how='inner', rsuffix='_x')
-    X.reset_index(drop=True, inplace=True)
-    output_df = X.ix[:, ['PRODUCT_LINE', 'ITEM_NUMBER', 'ITEM_RANK', 'BR_ID',
-                         'RANK_A', 'RANK_B', 'RANK_C', 'RANK_D', 'RANK_N']]
-    X.drop(['ITEM_NUMBER', 'ITEM_NUMBER_x',
+    index_list = ['ITEM_NUMBER', 'BR_ID']
+    X = pd.merge(a, b, how='inner', on=index_list)
+    output_df = X.ix[:, ['PRODUCT_LINE', 'ITEM_NUMBER', 'ITEM_RANK', 'BR_ID'
+                         ]]
+    X.drop(['ITEM_NUMBER',
             'ITEM_RANK', 'CM'], axis=1, inplace=True)
 
-    # X['L12-11_S'] = X.ix[:, 'L12M':'L11M'].sum(axis=1)
-    # X['L10-9_S'] = X.ix[:, 'L10M':'L9M'].sum(axis=1)
-    # X['L8-7M_S'] = X.ix[:, 'L9M':'L7M'].sum(axis=1)
-    # X['L6-5M_S'] = X.ix[:, 'L6M':'L5M'].sum(axis=1)
-    # X['L4-3_S'] = X.ix[:, 'L4M':'L3M'].sum(axis=1)
-    # X['L2-1_S'] = X.ix[:, 'L2M':'L1M'].sum(axis=1)
+    X['L12-11_S'] = X.ix[:, 'L12M':'L11M'].sum(axis=1)
+    X['L10-9_S'] = X.ix[:, 'L10M':'L9M'].sum(axis=1)
+    X['L8-7M_S'] = X.ix[:, 'L9M':'L7M'].sum(axis=1)
+    X['L6-5M_S'] = X.ix[:, 'L6M':'L5M'].sum(axis=1)
+    X['L4-3_S'] = X.ix[:, 'L4M':'L3M'].sum(axis=1)
+    X['L2-1_S'] = X.ix[:, 'L2M':'L1M'].sum(axis=1)
     #
-    # X['Q4_S'] = X.ix[:, 'L12M':'L10M'].sum(axis=1)
-    # X['Q3_S'] = X.ix[:, 'L9M':'L7M'].sum(axis=1)
-    # X['Q2_S'] = X.ix[:, 'L7M':'L4M'].sum(axis=1)
-    # X['Q1_S'] = X.ix[:, 'L3M':'L1M'].sum(axis=1)
+    X['Q4_S'] = X.ix[:, 'L12M':'L10M'].sum(axis=1)
+    X['Q3_S'] = X.ix[:, 'L9M':'L7M'].sum(axis=1)
+    X['Q2_S'] = X.ix[:, 'L7M':'L4M'].sum(axis=1)
+    X['Q1_S'] = X.ix[:, 'L3M':'L1M'].sum(axis=1)
     #
-    # X['L12-10_M'] = X.ix[:, 'L12M':'L11M'].mean(axis=1)
-    # X['L10-9_M'] = X.ix[:, 'L10M':'L9M'].mean(axis=1)
-    # X['L8-7_M'] = X.ix[:, 'L8M':'L7M'].mean(axis=1)
-    # X['L6-5_M'] = X.ix[:, 'L6M':'L5M'].mean(axis=1)
-    # X['L4-3_M'] = X.ix[:, 'L4M':'L3M'].mean(axis=1)
-    # X['L2-1_M'] = X.ix[:, 'L2M':'L1M'].mean(axis=1)
+    X['L12-10_M'] = X.ix[:, 'L12M':'L11M'].mean(axis=1)
+    X['L10-9_M'] = X.ix[:, 'L10M':'L9M'].mean(axis=1)
+    X['L8-7_M'] = X.ix[:, 'L8M':'L7M'].mean(axis=1)
+    X['L6-5_M'] = X.ix[:, 'L6M':'L5M'].mean(axis=1)
+    X['L4-3_M'] = X.ix[:, 'L4M':'L3M'].mean(axis=1)
+    X['L2-1_M'] = X.ix[:, 'L2M':'L1M'].mean(axis=1)
     #
-    # X['Q4_M'] = X.ix[:, 'L12M':'L10M'].mean(axis=1)
-    # X['Q3_M'] = X.ix[:, 'L9M':'L7M'].mean(axis=1)
-    # X['Q2_M'] = X.ix[:, 'L7M':'L4M'].mean(axis=1)
-    # X['Q1_M'] = X.ix[:, 'L3M':'L1M'].mean(axis=1)
-    # X['12M_M'] = X.ix[:, 'L12M':'L1M'].mean(axis=1)
+    X['Q4_M'] = X.ix[:, 'L12M':'L10M'].mean(axis=1)
+    X['Q3_M'] = X.ix[:, 'L9M':'L7M'].mean(axis=1)
+    X['Q2_M'] = X.ix[:, 'L7M':'L4M'].mean(axis=1)
+    X['Q1_M'] = X.ix[:, 'L3M':'L1M'].mean(axis=1)
+    X['12M_M'] = X.ix[:, 'L12M':'L1M'].mean(axis=1)
 
     y = X.pop('NM')
     return X, y, output_df
@@ -51,6 +51,10 @@ def round_number(series):
     return series.apply(lambda x: int(round(x)))
 
 if __name__ == '__main__':
+
+    path = "../forecast_data/"
+    dirs = sorted([f for f in os.listdir(path) if not f.startswith('.')])
+
     train_list = ['../sales_data/201511.csv',
                   '../sales_data/201512.csv',
                   '../sales_data/201601.csv',
@@ -72,7 +76,8 @@ if __name__ == '__main__':
         df_a = pd.read_csv(also_fit_path, low_memory=False)
         df_s = pd.read_csv(files[1], low_memory=False)
         X_test, y_test, output_df = transform(df_a, df_s)
-        model = RandomForestRegressor(n_estimators=10, n_jobs=-1)
+
+        model = RandomForestRegressor(n_estimators=500, n_jobs=-1)
         # model = GradientBoostingRegressor(n_estimators=1000)
 
         model.fit(X_train, y_train)
@@ -83,28 +88,30 @@ if __name__ == '__main__':
         msf = MSF()
         msf_pred = msf.predict(msf_X)
 
-        s = Score()
-        s.load_data(X_test, y_test)
-        print "MSF rmse_score: {}".format(s.rmse_score(msf_pred))
-        print "model rmse_score: {}".format(s.rmse_score(model_pred))
-        print
-        print "MSF r2_score: {}".format(s.r2_score(msf_pred))
-        print "model r2_score: {}".format(s.r2_score(model_pred))
-        print "model adj_r2_score: {}".format(s.adj_r2_score(model_pred))
-        print
+        # s = Score()
+        # s.load_data(X_test, y_test)
+        # print "MSF rmse_score: {}".format(s.rmse_score(msf_pred))
+        # print "model rmse_score: {}".format(s.rmse_score(model_pred))
+        # print
+        # print "MSF r2_score: {}".format(s.r2_score(msf_pred))
+        # print "model r2_score: {}".format(s.r2_score(model_pred))
+        # print "model adj_r2_score: {}".format(s.adj_r2_score(model_pred))
+        # print
         # s.hist(msf_pred,'MSF')
         # # s.hist(model_pred,'RF')
         # s.plot_3(msf_pred, model_pred)
         # plt.legend()
         # plt.show()
+
         output_df['{}_ACTUAL'.format(predict_date)] = y_test
         output_df['{}_MSF_FORECAST'.format(
             predict_date)] = round_number(msf_pred)
         output_df['{}_RF_FORECAST'.format(
             predict_date)] = round_number(model_pred)
-        # print output_df.head()
+
         output_df.to_csv(
             '../forecast_data/{}_forecast.csv'.format(predict_date), mode='w', index=False)
+
         # s.item_rank_score(msf_pred, model_pred)
 
         # Display top 30 important features
--- code/measurement.py
+++ code/measurement.py
@@ -1,23 +1,22 @@
 import os
-import sys
 
+import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
     path = "../forecast_data/"
+    index_list = ['PRODUCT_LINE', 'ITEM_NUMBER', 'ITEM_RANK', 'BR_ID']
+    weight_dic = {'A': 3, 'B': 2, 'C': 1, 'D': 1, 'N': 3}
     dirs = sorted([f for f in os.listdir(path) if not f.startswith('.')])
 
-    df_list = []
-    for f in dirs:
-        df_list.append(pd.read_csv('{}/{}'.format(path, f)))
+    result = pd.read_csv('{}/{}'.format(path, dirs[0]))
+    result.set_index(
+        keys=[result.ITEM_NUMBER, result.BR_ID], drop=False, inplace=True)
 
-    for df in df_list:
-        df.set_index(keys=[df.ITEM_NUMBER, df.BR_ID], drop=False, inplace=True)
-    result = df_list[0].join(df_list[1], how='inner', rsuffix='_x')
-    for df in df_list[2:]:
-        result = result.join(df, how='inner', rsuffix='_x')
-    result.reset_index(drop=True, inplace=True)
-    result.drop([x for x in result.columns if '_x' in x], axis=1, inplace=1)
+    for f in dirs[1:]:
+        df = pd.read_csv('{}/{}'.format(path, f))
+        result = pd.merge(result, df, how='inner', on=index_list)
+
     actual = [x for x in result.columns if 'ACTUAL' in x]
     MSF = [x for x in result.columns if 'MSF' in x]
     RF = [x for x in result.columns if 'RF' in x]
@@ -26,7 +25,9 @@ if __name__ == '__main__':
         axis=1) - result.ix[:, MSF].sum(axis=1)) / len(actual)
     result['RF_AE'] = (result.ix[:, actual].sum(axis=1) -
                        result.ix[:, RF].sum(axis=1)) / len(actual)
+
     result['MSF_MAD'], result['RF_MAD'] = 0, 0
+
     for col in zip(actual, MSF):
         result['MSF_MAD'] += abs(result[col[0]] - result[col[1]])
     result['MSF_MAD'] /= len(dirs)
@@ -34,10 +35,13 @@ if __name__ == '__main__':
     for col in zip(actual, RF):
         result['RF_MAD'] += abs(result[col[0]] - result[col[1]])
     result['RF_MAD'] /= len(dirs)
-    result['WEIGHT'] = 1
 
-    result['WEIGHT'] = result.apply(lambda x: 4 if x['RANK_A'] == 1 else 3 if x[
-                                    'RANK_B'] == 1 else 2 if x['RANK_C'] == 1 else 1, axis=1)
+    result['WEIGHT'] = 1
+    result['WEIGHT'] = result.apply(
+        lambda x: weight_dic[x['ITEM_RANK']], axis=1)
 
     result['WEIGHT_MSF_MAD'] = result['WEIGHT'] * result['MSF_MAD']
     result['WEIGHT_RF_MAD'] = result['WEIGHT'] * result['RF_MAD']
+
+    result_pv = pd.pivot_table(result, values=['MSF_AE', 'RF_AE', 'MSF_MAD', 'RF_MAD', 'WEIGHT_MSF_MAD', 'WEIGHT_RF_MAD'], index=[
+                               'PRODUCT_LINE', 'ITEM_RANK'], aggfunc=(np.mean, np.std))
